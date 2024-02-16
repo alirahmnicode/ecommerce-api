9af6bd832107fd7c6caba7ab4f7b4554174ec018
alirahmanicode <alirahmanicode@gmail.com>
2024-02-16 12:47:37 +0330

    update

--- storefront/urls.py
+++ storefront/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static 
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularRedocView,
@@ -35,3 +37,5 @@ urlpatterns = [
 
 
 urlpatterns += doc_patterns
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
