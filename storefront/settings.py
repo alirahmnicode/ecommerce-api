7de744d4581bb25b6000b0b76283b76e55aaa3ce
alirahmanicode <alirahmanicode@gmail.com>
2024-04-06 17:50:16 +0330

    update

--- storefront/settings.py
+++ storefront/settings.py
@@ -171,14 +171,3 @@ SPECTACULAR_SETTINGS = {
 }
 
 CORS_ALLOW_ALL_ORIGINS = True
-
-
-CACHES = {
-    "default": {
-        "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",
-        "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient",
-        },
-    }
-}
