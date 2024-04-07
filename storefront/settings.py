4eb87d88656f5c6e5d324214797bfce2f920fa0b
alirahmanicode <alirahmanicode@gmail.com>
2024-04-07 19:42:48 +0330

    update

--- storefront/settings.py
+++ storefront/settings.py
@@ -80,25 +80,25 @@ WSGI_APPLICATION = "storefront.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.mysql",
-#         "NAME": "webstorepr$default",
-#         "HOST": "webstorepr.mysql.pythonanywhere-services.com",
-#         "USER": "webstorepr",
-#         "PASSWORD": "adminali0921",
-#     }
-# }
-
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
-        "NAME": "storefront",
-        "HOST": "localhost",
-        "USER": "root",
+        "NAME": "webstorepr$default",
+        "HOST": "webstorepr.mysql.pythonanywhere-services.com",
+        "USER": "webstorepr",
         "PASSWORD": "adminali0921",
     }
 }
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.mysql",
+#         "NAME": "storefront",
+#         "HOST": "localhost",
+#         "USER": "root",
+#         "PASSWORD": "adminali0921",
+#     }
+# }
 
 
 # Password validation
